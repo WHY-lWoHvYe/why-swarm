e14fad50be3f9a57409bc85b7d300d7c88ec8119
王红岩 lWoHvYe <lwohvye@outlook.com>
2022-07-28 21:13:06 +0800

    Rest/Feign请求前，Do Something

---
+++ why-sys-adaptor/src/main/java/com/lwohvye/sysadaptor/config/FeignInterceptorConfig.java
@@ -0,0 +1,23 @@
+package com.lwohvye.sysadaptor.config;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 有定义Bean和实现接口两种方式
+ *
+ * @date 2022/7/28 8:58 PM
+ */
+@Configuration
+public class FeignInterceptorConfig {
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            // Do Something before feign-call
+            // 这里是Support Map类型的参数的
+            requestTemplate.header("TNT", "Boom!!");
+        };
+    }
+}
---
+++ why-sys-adaptor/src/main/java/com/lwohvye/sysadaptor/config/RestTemplateSimInterceptor.java
@@ -0,0 +1,17 @@
+package com.lwohvye.sysadaptor.config;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+public class RestTemplateSimInterceptor implements ClientHttpRequestInterceptor {
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        // Do Something before call the restAPI
+        request.getHeaders().add("Ypa", "You know who");
+        return execution.execute(request, body);
+    }
+}
