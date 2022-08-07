8c1b4749fe741453a8acd4b081374a01f2f8a30d
王红岩 lWoHvYe <lwohvye@outlook.com>
2022-08-07 23:41:53 +0800

    Fixed `Method has too many Body parameters` with feign-httpclient

--- why-sys-adaptor/src/main/java/com/lwohvye/sysadaptor/service/ISysResourceFeignClientService.java
+++ why-sys-adaptor/src/main/java/com/lwohvye/sysadaptor/service/ISysResourceFeignClientService.java
@@ -3,16 +3,26 @@ package com.lwohvye.sysadaptor.service;
 import com.lwohvye.annotation.log.Log;
 import com.lwohvye.api.modules.system.api.SysResourceAPI;
 import com.lwohvye.api.modules.system.domain.Resource;
+import com.lwohvye.api.modules.system.service.dto.ResourceQueryCriteria;
 import com.lwohvye.sysadaptor.service.fallback.SysResourceFallbackFactory;
 import com.lwohvye.utils.result.ResultInfo;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import java.util.Map;
+
 @FeignClient(value = "why-system", contextId = "resourceValentine", fallbackFactory = SysResourceFallbackFactory.class)
 public interface ISysResourceFeignClientService extends SysResourceAPI {
+
+    @GetMapping("/api/sys/resources")
+    @Override
+    ResponseEntity<ResultInfo<Map<String, Object>>> query(@SpringQueryMap ResourceQueryCriteria criteria, Pageable pageable);
 
     @Log("新增资源")
     @PostMapping("/api/sys/resources")
--- why-sys-adaptor/src/main/java/com/lwohvye/sysadaptor/service/ISysRoleFeignClientService.java
+++ why-sys-adaptor/src/main/java/com/lwohvye/sysadaptor/service/ISysRoleFeignClientService.java
@@ -3,18 +3,27 @@ package com.lwohvye.sysadaptor.service;
 import com.lwohvye.annotation.log.Log;
 import com.lwohvye.api.modules.system.api.SysRoleAPI;
 import com.lwohvye.api.modules.system.domain.Role;
+import com.lwohvye.api.modules.system.service.dto.RoleQueryCriteria;
 import com.lwohvye.sysadaptor.service.fallback.SysRoleFallbackFactory;
 import com.lwohvye.utils.result.ResultInfo;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import java.util.Map;
 import java.util.Set;
 
 @FeignClient(value = "why-system", contextId = "roleValentine", fallbackFactory = SysRoleFallbackFactory.class)
 public interface ISysRoleFeignClientService extends SysRoleAPI {
+
+    @GetMapping("/api/sys/roles")
+    @Override
+    ResponseEntity<ResultInfo<Map<String, Object>>> query(@SpringQueryMap RoleQueryCriteria criteria, Pageable pageable);
 
     @Log("新增角色")
     @PostMapping("/api/sys/roles")
